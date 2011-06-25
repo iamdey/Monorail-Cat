ea33d80d07d620eb70c1cf127bc2dcdb3cce8960
David Epely <esion99@gmail.com>
2011-06-25 16:25:49 +0200

    add prototype cat controller

--- libs/game.js
+++ libs/game.js
@@ -1,1 +1,11 @@
-function Game(gs) { }
+function Game(gs) {
+	var c = gs.c;
+	gs.addEntity(new Player(gs));
+}
+
+function startGame() {
+    var surface = document.getElementById("monorail-cat");
+    var gs 		= new JSGameSoup(surface, 30);
+    var game 	= new Game(gs);
+    gs.launch();
+}
