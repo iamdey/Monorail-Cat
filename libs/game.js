729faa9fd6212774663088e7e967f8016916018c
zouip <zouip.flon@yopmail.com>
2011-06-26 16:28:18 +0200

    changement pour permettre chargement de maps

--- libs/game.js
+++ libs/game.js
@@ -3,11 +3,11 @@ var debug 		= false;
 
 tilemap = null;
 
-function loadMap() {
+function loadMap(mapName) {
 	tilemap = new TileMap(document.getElementById("tileMap"));
-	tilemap.loadCsv("144;66;66;219;66;66;9\n1056;144;66;2886;66;9;1056\n1056;1056;144;66;9;1056;1056\n1056;3504;1581;0;3504;1581;1056\n1056;1056;2304;66;516;1056;1056\n1056;2304;66;219;66;516;1056\n2304;66;66;2886;66;66;516\n");
+	tilemap.load(mapName);
 	//tilemap.load('zouip');
-	setTimeout('tilemap.draw(document.getElementById("tileMap"))', 500);
+	setTimeout('tilemap.draw(document.getElementById("tileMap"))', 300);
 }
 
 function startGame() {
@@ -35,7 +35,7 @@ function Game(gs) {
 	};
 
 	var map = new Map(tilemap);
-	
+
 	var mapItems = [
 		new MapItem("6_0", map, 6, 0),
 		new MapItem("0_6", map, 0, 6),
@@ -44,27 +44,27 @@ function Game(gs) {
 		new MapItem("2_2", map, 4, 2),
 		new MapItem("4_4", map, 2, 4)
 	];
-	
+
 	for (var i = 0; i < mapItems.length; i++) {
 		map.addEntity(mapItems[i]);
 		gs.addEntity(mapItems[i]);
 	}
-	
+
 	var cats = [
 		new Cat(map, 1, RED, 0, 0, SOUTH),
 		new Cat(map, 2, BLUE, 6, 6, NORTH)
 	];
-	
+
 	var players = [
 		new Player("player 1", this.keymap_player_1, cats[0]),
 		new Player("player 2", this.keymap_player_2, cats[1])
 	];
-	
+
 	gs.addEntity(players[0]);
 	gs.addEntity(players[1]);
 	gs.addEntity(cats[0]);
 	gs.addEntity(cats[1]);
-	
+
 	map.addEntity(cats[0]);
 	map.addEntity(cats[1]);
 }
